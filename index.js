fb1ca96fa3bf5dfbf0706f30292b7b7609c640a9
parthajmera05 <ajmeraparth05@gmail.com>
2024-02-04 13:39:11 +0530

    Attached with backend

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
+import express from "express" ; 
+import bodyParser from "body-parser";
+import path, { dirname } from "path" ; 
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url)) ; 
 
-const swiper = new Swiper('.swiper', {
-    
-    direction: 'horizontal',
-    loop: true,
-    allowTouchMove:false,
-    centeredSlides: true,
-    
-  
-   
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-   
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    autoplay: {
-        delay: 2000,
-      },
-  
-   
-   
-  });
+const app = express() ; 
+const port = 3000 ; 
+app.use(bodyParser.urlencoded({extended : true})) ; 
+app.set('views', path.join(__dirname + '/views'));
+app.set('view engine' , 'ejs');
+app.use("/static" , express.static(__dirname +"/public")) ;
+app.get("/" , (req,res) => {
+    res.render("index.ejs") ; 
+}) ;
+
+
+
+app.listen(port , () => {
+    console.log(`Server is listening on port ${port}`) ; 
+}) ;
